9c232e0c25bf4c75bde999e425fb2294c6c166e1
Jack Wheeler <jack.wheeler@rogers.com>
2022-09-28 11:59:06 -0400

    Initial setup complete

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const port = 8000
+express()
+    .use(helmet())
+    .use(morgan('tiny'))
+    
+    .get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+
+    .listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
