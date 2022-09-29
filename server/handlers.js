930f8c68db8e7e9943aaaf13bc624197cc98c435
Jack Wheeler <jack.wheeler@rogers.com>
2022-09-29 13:58:42 -0400

    Post and Get topping handlers

---
+++ server/handlers.js
@@ -0,0 +1,89 @@
+const { MongoClient } = require("mongodb");
+const { v4: uuidv4 } = require("uuid");
+
+require("dotenv").config();
+const { MONGO_URI } = process.env
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+const addTopping = async (req, res) => {
+    
+    const client = new MongoClient(MONGO_URI, options)
+    try {
+        const {name, price, isSpicy, isVegan, isGlutenFree} = req.body
+        await client.connect()
+        const db = client.db("Ultimate-Pizza")
+
+        existsAlready = await db.collection("toppings").findOne({name: name.toLowerCase()})
+        if (!existsAlready) {
+            await db.collection("toppings").insertOne({_id: uuidv4(), name: name.toLowerCase(), price, isSpicy, isVegan, isGlutenFree})
+        } else {
+            res.status(400).json({
+                status: 400,
+                message: "Topping already exists",
+                data: req.body
+            })
+        }
+        res.status(200).json({
+            status: 200,
+            message: "Topping added"
+        })
+    } catch (err) {
+        res.status(500).json({
+            status: 500,
+            message: "Error posting topping" + err.message,
+            data: req.body
+        })
+    } finally {
+        await client.close()
+    }
+    
+}
+
+const getTopping = async (req, res) => {
+    const client = new MongoClient(MONGO_URI, options)
+    try {
+        const toppingName = req.params.toppingName
+
+        await client.connect()
+        const db = client.db("Ultimate-Pizza")
+
+        if (!toppingName) {
+            const toppings = await db.collection("toppings").find().toArray()
+            res.status(200).json({
+                status: 200,
+                message: toppings
+            })
+        } else {
+            const reqTopping = await db.collection("toppings").findOne({name: toppingName.toLowerCase()})
+
+        reqTopping
+        ? res.status(200).json({
+            status: 200,
+            message: reqTopping
+        })
+        : res.status(400).json({
+            status: 400,
+            message: "Requested topping does not exist"
+        })
+        }
+
+        
+        
+    } catch (err) {
+        res.status(500).json({
+            status: 500,
+            message: "Error retrieving topping" + err.message
+        })
+    } finally {
+        await client.close()
+    }
+    
+}
+
+module.exports = {
+    addTopping,
+    getTopping,
+};  
--- server/server.js
+++ server/server.js
@@ -1,15 +1,38 @@
 const express = require('express')
 const helmet = require('helmet')
 const morgan = require('morgan')
+const { addTopping,
+        getTopping, } = require("./handlers")
 const port = 8000
 express()
     .use(helmet())
     .use(morgan('tiny'))
     .use(express.json())
     
-    .get('/', (req, res) => {
-        res.send('Hello World!')
+    //Admin Post
+    .post("/admin/add-topping", addTopping)
+
+
+
+
+    .get("/get-topping/:toppingName?", getTopping)
+
+
+
+
+
+
+
+
+    //Misc. Handlers
+    .get("*", (req, res) => {
+        res.status(404).json({
+            status: 404,
+            message: "Page not found"
+        })
     })
+
+
 
     .listen(port, () => {
         console.log(`Example app listening on port ${port}`)
